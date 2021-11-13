9b712ba49f70518adab46a9efc85d7222dd6f795
weiweixiong <1092486722@qq.com>
2021-11-13 19:25:43 +0800

    feat: 项目初始化构建

---
+++ gulimall-ware/src/test/java/com/wwxyh/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwxyh.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

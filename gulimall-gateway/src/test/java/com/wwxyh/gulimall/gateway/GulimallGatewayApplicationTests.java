f147f9c7f76d9393942ab9f977084a6ab3b8aca6
weiweixiong <1092486722@qq.com>
2021-11-26 23:12:51 +0800

    feat: gateway网关添加

---
+++ gulimall-gateway/src/test/java/com/wwxyh/gulimall/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwxyh.gulimall.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

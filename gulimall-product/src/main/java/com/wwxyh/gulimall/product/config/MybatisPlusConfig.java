e187a08699102f00e3f99afe0af4a072d7f41335
weiweixiong <1092486722@qq.com>
2022-01-06 22:32:37 +0800

    feat: mybatis-plus主键策略设置、分页插件添加

---
+++ gulimall-product/src/main/java/com/wwxyh/gulimall/product/config/MybatisPlusConfig.java
@@ -0,0 +1,32 @@
+package com.wwxyh.gulimall.product.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Description: Mybatis-Plus配置
+ * @Author: wwx
+ * @Date: 2022/1/6 22:15
+ */
+@Configuration
+@EnableTransactionManagement  // 开启事务
+@MapperScan("com.wwxyh.gulimall.product.entity")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        paginationInterceptor.setOverflow(true);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        paginationInterceptor.setLimit(1000);
+        return paginationInterceptor;
+    }
+}
